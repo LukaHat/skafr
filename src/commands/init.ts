4ff13380cfa227fbd6c85c66c7d92ac1f637260b
lukahat <luka.hat58@gmail.com>
2026-04-25 11:52:54 +0200

    feat: generate base project files and run npm install on init

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,8 +1,10 @@
 import { InitOptions } from "../types";
-import { mkdirSync, existsSync, writeFileSync, rmSync } from "fs";
+import { mkdirSync, existsSync, writeFileSync, rmSync, readFileSync } from "fs";
 import { confirm } from "@inquirer/prompts";
 import { join } from "path";
 import { cwd } from "process";
+import { generatePackageJSON } from "../generators/initGenerator";
+import { spawnSync } from "child_process";
 
 export const initCommand = async (
   projectName: string,
@@ -42,6 +44,81 @@ export const initCommand = async (
       join(cwd(), projectName, ".skafc"),
       JSON.stringify(options, null, 2),
     );
+
+    const packageJSONRaw = generatePackageJSON(projectName, options);
+
+    writeFileSync(
+      join(cwd(), projectName, "package.json"),
+      JSON.stringify(packageJSONRaw, null, 2),
+    );
+
+    const app = readFileSync(
+      join(__dirname, "..", "templates", "express", "init", "app.ts.template"),
+      "utf-8",
+    );
+
+    writeFileSync(join(cwd(), projectName, "src", "app.ts"), app);
+
+    const server = readFileSync(
+      join(
+        __dirname,
+        "..",
+        "templates",
+        "express",
+        "init",
+        "server.ts.template",
+      ),
+      "utf-8",
+    );
+
+    writeFileSync(join(cwd(), projectName, "src", "server.ts"), server);
+
+    const config = readFileSync(
+      join(
+        __dirname,
+        "..",
+        "templates",
+        "express",
+        "init",
+        "config.ts.template",
+      ),
+      "utf-8",
+    );
+
+    writeFileSync(join(cwd(), projectName, "src", "config.ts"), config);
+
+    const envExample = readFileSync(
+      join(
+        __dirname,
+        "..",
+        "templates",
+        "express",
+        "init",
+        ".env.example.template",
+      ),
+      "utf-8",
+    );
+
+    writeFileSync(join(cwd(), projectName, ".env.example"), envExample);
+
+    const tsConfig = readFileSync(
+      join(
+        __dirname,
+        "..",
+        "templates",
+        "express",
+        "init",
+        "tsconfig.json.template",
+      ),
+      "utf-8",
+    );
+
+    writeFileSync(join(cwd(), projectName, "tsconfig.json"), tsConfig);
+
+    spawnSync("npm", ["install"], {
+      cwd: join(cwd(), projectName),
+      stdio: "inherit",
+    });
   } catch (error) {
     throw new Error(`Failed to scaffold project: ${(error as Error).message}`);
   }
---
+++ src/generators/initGenerator.ts
@@ -0,0 +1,51 @@
+import { InitOptions, SupportedDBs, SupportedOrms } from "../types";
+
+export const generatePackageJSON = (
+  projectName: string,
+  options: InitOptions,
+) => {
+  return {
+    name: projectName,
+    version: "1.0.0",
+    scripts: {
+      dev: "nodemon --exec ts-node src/server.ts",
+      build: "tsc",
+      start: "node dist/server.js",
+      test: 'echo "Error: no test specified" && exit 1',
+    },
+    dependencies: {
+      cors: "^2.8.6",
+      dotenv: "^17.3.1",
+      express: "^5.2.1",
+      inversify: "^7.11.0",
+      zod: "^4.3.6",
+      "reflect-metadata": "^0.2.2",
+
+      ...(options.auth && {
+        argon2: "^0.44.0",
+        jsonwebtoken: "^9.0.2",
+      }),
+
+      ...(options.db === SupportedDBs.postgres && {
+        pg: "^8.19.0",
+        "pg-hstore": "^2.3.4",
+      }),
+
+      ...(options.orm === SupportedOrms.sequelize && {
+        sequelize: "^6.37.7",
+      }),
+    },
+    devDependencies: {
+      "@types/express": "^5.0.6",
+      "@types/node": "^25.3.0",
+      nodemon: "^3.1.14",
+      "ts-node": "^10.9.2",
+      typescript: "^5.9.3",
+      "@types/cors": "^2.8.19",
+
+      ...(options.auth && {
+        "@types/jsonwebtoken": "^9.0.10",
+      }),
+    },
+  };
+};
